f2d643b0799cb86a3c0a336546a1ac3da36fd4ed
Priyatham <kunarapupriyatham@gmail.com>
2022-06-08 13:46:26 +0530

    Update parent_window.py

--- Samples/Simple_Demo/Python_Export/Export_whole_project/parent_window.py
+++ Samples/Simple_Demo/Python_Export/Export_whole_project/parent_window.py
@@ -1,6 +1,5 @@
 import os
 from delphifmx import *
-from child_window import Child_Form
 
 class Parent_Form(Form):
 
@@ -10,8 +9,6 @@ class Parent_Form(Form):
         self.enter_text_label = None
         self.main_heading = None
         self.LoadProps(os.path.join(os.path.dirname(os.path.abspath(__file__)), "parent_window.pyfmx"))
-        self.child_form = Child_Form(self)
 
     def my_buttonClick(self, Sender):
-        self.child_form.result_text_label.Text = self.enter_text_edit.Text
-        self.child_form.show()
+        pass
